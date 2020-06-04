773fdd9f071110984b2f5c46d1941dcc82248981
oshhh <osheen18059@iiitd.ac.in>
2020-06-04 23:20:08 +0530

    merging login with resource picker

--- sdk/test/resource_picker_test.dart
+++ sdk/test/resource_picker_test.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sdk/functionality/resource_picker.dart';
+import 'package:sdk/services/resource_picker.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
