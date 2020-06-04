057a4bd222476ed996bc570319e4d86676eb9143
oshhh <osheen18059@iiitd.ac.in>
2020-06-05 01:15:52 +0530

    test 3 added

--- sdk/test/resource_picker_test.dart
+++ sdk/test/resource_picker_test.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:googleapis_auth/auth.dart';
 import 'package:mockito/mockito.dart';
@@ -37,7 +34,7 @@ void main() {
     expect(resourcePicker.accessToken, "accessTokenTest");
     expect(resourcePicker.refreshToken, "refreshTokenTest");
   });
-  test('test 1: API function throws error', () async {
+  test('test 2: API function throws error', () async {
     MockAuthClient mockAuthClient = new MockAuthClient();
     MockAccessCredentials mockAccessCredentials = new MockAccessCredentials();
     MockAccessToken mockAccessToken = new MockAccessToken();
@@ -55,6 +52,29 @@ void main() {
 
     // Expected behaviour
     ResourcePicker resourcePicker = new ResourcePicker.test("client_id", "client_secret", "enterprise_id", "redirect_url", mockClientViaUserConsent);
+    expect(resourcePicker.resourcePickerURL, "https://accounts.google.com/o/oauth2/auth?client_id=client_id&redirect_uri=redirect_url&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsdm.service&state=state");
+    expect(await resourcePicker.askForAuthorization(), "authorization failed");
+  });
+
+  test('test 3: API function takes longer than timeout set', () async {
+    MockAuthClient mockAuthClient = new MockAuthClient();
+    MockAccessCredentials mockAccessCredentials = new MockAccessCredentials();
+    MockAccessToken mockAccessToken = new MockAccessToken();
+    MockAccessToken mockRefreshToken = new MockAccessToken();
+
+    when(mockAuthClient.credentials).thenReturn(mockAccessCredentials);
+    when(mockAccessCredentials.accessToken).thenReturn(mockAccessToken);
+    when(mockAccessCredentials.refreshToken).thenReturn("refreshTokenTest");
+    when(mockAccessToken.data).thenReturn("accessTokenTest");
+
+    // Defining behaviour: throws an error on calling
+    Future<AuthClient> mockClientViaUserConsent(ClientId clientId, List<String> scope, Function launchFunction()) async {
+      await Future.delayed(new Duration(milliseconds: 200));
+      return mockAuthClient;
+    }
+
+    // Expected behaviour
+    ResourcePicker resourcePicker = new ResourcePicker.test("client_id", "client_secret", "enterprise_id", "redirect_url", mockClientViaUserConsent, resourcePickerTimeoutDuration: new Duration(milliseconds: 100));
     expect(resourcePicker.resourcePickerURL, "https://accounts.google.com/o/oauth2/auth?client_id=client_id&redirect_uri=redirect_url&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsdm.service&state=state");
     expect(await resourcePicker.askForAuthorization(), "authorization failed");
   });
