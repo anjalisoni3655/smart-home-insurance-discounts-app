bfb10b5baaf50417f12d3c0959ae357fc08cf1b3
oshhh <osheen@google.com>
2020-06-06 01:19:24 +0530

    resolved conflicts

--- sdk/test/sdk_test.dart
+++ sdk/test/sdk_test.dart
@@ -3,10 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sdk/sdk.dart';
 
 void main() {
-<<<<<<< HEAD
   test('test 1', () {});
-=======
-  test('test 1', () {
-  });
->>>>>>> 8d8f7b8a494764e29c76de156a9c8de3eab9a310
 }
