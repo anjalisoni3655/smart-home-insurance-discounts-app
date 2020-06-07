8ca9a08bcf0c768301bf76043679696e34c7692d
khushi2305 <guptakhushi2402@gmail.com>
2020-06-07 17:13:09 +0530

    Displayed Discount offers

--- home_insurance_app/lib/data/company_offers.dart
+++ home_insurance_app/lib/data/company_offers.dart
@@ -1,10 +1,11 @@
 import 'package:homeinsuranceapp/data/offer.dart';
 
-// This class contains all teh discounts offered by the insurance company 
+// This class contains all teh discounts offered by the insurance company
 class CompanyOffers {
   int noOfOffers = 7;
- List<Offer> availableOffers = [
-    new Offer({'Nest Hello Doorbell': 1, 'Nest Protect - Smoke + CO alarm': 1}, 50),
+  List<Offer> availableOffers = [
+    new Offer(
+        {'Nest Hello Doorbell': 1, 'Nest Protect - Smoke + CO alarm': 1}, 50),
     new Offer({'Nest Cam IQ Outdoor': 1, 'Nest Thermostat': 1}, 45),
     new Offer({'Nest Protect- Smoke + CO alarm': 2}, 38),
     new Offer({'Nest X Yale Lock': 1, 'Nest Hello Doorbell': 1}, 18),
--- home_insurance_app/lib/pages/show_discounts.dart
+++ home_insurance_app/lib/pages/show_discounts.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:homeinsuranceapp/data/company_offers.dart';
 
 Map data;
-CompanyOffers offers= new CompanyOffers();
+CompanyOffers offers = new CompanyOffers();
 
 class DisplayDiscounts extends StatefulWidget {
   @override
@@ -11,11 +11,13 @@ class DisplayDiscounts extends StatefulWidget {
 
 class DisplayDiscountsState extends State<DisplayDiscounts> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
     print(offers.noOfOffers);
     print("jkwxkc");
   }
+
+  void iterate(Map<String, int> m) {}
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
@@ -44,33 +46,83 @@ class DisplayDiscountsState extends State<DisplayDiscounts> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
               child: Text(
-                'Available Discount Offers',
+                'Available Policies',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 30.0,
+                  fontFamily: 'PTSerifBI',
                 ),
               ),
+            ),
+            const Divider(
+              color: Colors.brown,
+              height: 10.0,
+              thickness: 5,
+              indent: 5,
+              endIndent: 5,
             ),
             SizedBox(height: 50.0),
             Expanded(
               child: ListView.builder(
-                  itemCount: 10,//CompanyOffers.availableOffers.length , // available offers is static variable
+                  itemCount: offers.availableOffers
+                      .length, // available offers is static variable
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 1.0, horizontal: 2.0),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 10.0),
                       child: Card(
-                        child: ListTile(
-                            onTap: () {},
-                            title: Text(
-                                '${offers.availableOffers[index].discount}'),
-                            leading: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.dehaze,
-                                color: Colors.lightBlue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          side: BorderSide(
+                            color: Colors.green,
+                            width: 5.0,
+                          ),
+                        ),
+                        //  color: Colors.lightBlueAccent[100],
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 10,
+                              child: Column(
+                                  children: (offers.availableOffers[index])
+                                      .requirements
+                                      .entries
+                                      .map(
+                                        (entry) => Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Container(
+                                              margin: EdgeInsets.all(0.0),
+                                              padding: EdgeInsets.all(10.0),
+                                              decoration: BoxDecoration(
+                                                  // color: Colors.lightBlueAccent[100],
+                                                  ),
+                                              child: Text(
+                                                '${entry.key} : ${entry.value}',
+                                                textAlign: TextAlign.left,
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontFamily: "PTSerifBI",
+                                                  fontSize: 17,
+                                                ),
+                                              )),
+                                        ),
+                                      )
+                                      .toList()),
+                            ),
+                            Expanded(
+                              flex:2,
+                              child: Text(
+                                '${offers.availableOffers[index].discount} %',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20.0,
+                                ),
                               ),
-                            )),
+                              
+                            )
+                          ],
+                        ),
                       ),
                     );
                   }),
