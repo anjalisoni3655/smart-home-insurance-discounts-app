cc843c3b48bd75e21e58189affed76f4c3127c6c
anjalisoni3655 <anjalisony32@gmail.com>
2020-06-09 09:52:31 +0530

    removed comments

--- home_insurance_app/lib/components/rounded_buttons.dart
+++ home_insurance_app/lib/components/rounded_buttons.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // this widget is used to get the round shaped buttons in the login screen
 // these buttons can be used anywhere else in the code by just providing the parameters
 // title,color, and onpressed as arguments
-//It's done to make the code less bulky
+
 class RoundedButton extends StatelessWidget {
   RoundedButton({this.title, this.colour, @required this.onPressed});
 
