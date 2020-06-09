b2a66f28f077ad9e5c7c8f70e08edd10625ce072
anjalisoni3655 <anjalisony32@gmail.com>
2020-06-09 16:35:08 +0530

    resolved small changes

--- home_insurance_app/lib/pages/login_screen.dart
+++ home_insurance_app/lib/pages/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:homeinsuranceapp/pages/home.dart';
 
-// widget for the login with google
+// widget for login with google
 class LoginScreen extends StatefulWidget {
   static const String id = '/';
 
