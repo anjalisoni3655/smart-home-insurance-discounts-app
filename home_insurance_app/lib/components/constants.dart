2529101253915e069c712c5fcf9298d7dd994264
anjalisoni3655 <anjalisony32@gmail.com>
2020-06-11 12:37:31 +0530

    made a css file

---
+++ home_insurance_app/lib/components/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+/// TextStyle for the Profile Name in profile page
+const kProfileNameStyle = TextStyle(
+  fontFamily: 'Pacifico',
+  fontSize: 40.0,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+
+/// TextStyle for the Designation Name in the Profile page
+const kDesignationTextStyle = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  color: Colors.black,
+  fontSize: 20.0,
+  letterSpacing: 2.5,
+  fontWeight: FontWeight.bold,
+);
+
+
+/// Normal TextStyle for various usages.
+/// [color: Black, fontFamily: Source Sans Pro, size: 20.0]
+const kNormalTextStyle = TextStyle(
+  color: Colors.black,
+  fontFamily: 'Source Sans Pro',
+  fontSize: 20.0,
+);
+
--- home_insurance_app/lib/pages/profile.dart
+++ home_insurance_app/lib/pages/profile.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:homeinsuranceapp/components/constants.dart';
 
 //widget for the my profile page displaying user's details
-
 class Profile extends StatelessWidget {
   static const String id = 'profile';
   @override
@@ -12,71 +12,53 @@ class Profile extends StatelessWidget {
         backgroundColor: Colors.white70,
         body: SafeArea(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('assets/user.png'),
-            ),
-            Text(
-              'Mr. XYZ',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Business Analyst',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.black,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 150.0,
-              child: Divider(
-                color: Colors.black,
-              ),
-            ),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/user.png'),
+                ),
+                Text(
+                  'Mr. XYZ',
+                  style: kProfileNameStyle,
+                ),
+                Text(
+                  'Business Analyst',
+                  style: kDesignationTextStyle,
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
                     color: Colors.black,
                   ),
-                  title: Text(
-                    '+916203444943',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                )),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.black,
-                  ),
-                  title: Text(
-                    'xyz@email.com',
-                    style: TextStyle(
-                        fontSize: 20.0,
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
                         color: Colors.black,
-                        fontFamily: 'Source Sans Pro'),
-                  ),
-                ))
-          ],
-        )),
+                      ),
+                      title: Text(
+                        '+91xxxxxxxxxx',
+                        style: kNormalTextStyle,
+                      ),
+                    )),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'xyz@email.com',
+                        style: kNormalTextStyle,
+                      ),
+                    ))
+              ],
+            )),
       ),
     );
   }
