ed303450904c0133361c109c0ca06db090d1ae3c
khushi2305 <guptakhushi2402@gmail.com>
2020-06-12 00:47:29 +0530

    Added comments for describing class and widgets

--- home_insurance_app/lib/pages/choose_policy.dart
+++ home_insurance_app/lib/pages/choose_policy.dart
@@ -52,7 +52,7 @@ class _DisplayPoliciesState extends State<DisplayPolicies> {
                   ),
                 ),
               ),
-              CustomDivider(
+              CustomDivider(  // Returns a Divider widget whose some attributes are defined by the parameters and others take the default value
                   height: screenheight / 100, width: screenwidth / 100),
               CustomSizedBox(height: screenheight / 100),
               RadioGroup(data),
--- home_insurance_app/lib/pages/show_discounts.dart
+++ home_insurance_app/lib/pages/show_discounts.dart
@@ -4,7 +4,7 @@ class DisplayDiscounts extends StatefulWidget {
   @override
   DisplayDiscountsState createState() => DisplayDiscountsState();
 }
-
+//This class gives the overall layout for the show discounts page .
 class DisplayDiscountsState extends State<DisplayDiscounts> {
   @override
   Widget build(BuildContext context) {
