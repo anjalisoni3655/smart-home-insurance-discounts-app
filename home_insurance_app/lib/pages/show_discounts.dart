6ec662f9b00ae5c7451c3bdeb885cc80bc41c7b9
khushi2305 <guptakhushi2402@gmail.com>
2020-06-11 19:07:29 +0530

    Used custom widgets

--- home_insurance_app/lib/pages/show_discounts.dart
+++ home_insurance_app/lib/pages/show_discounts.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:homeinsuranceapp/data/company_database.dart';
 import 'package:homeinsuranceapp/data/offer.dart';
 import 'package:homeinsuranceapp/pages/common_widgets.dart';
+import 'package:homeinsuranceapp/pages/style/custom_widgets.dart';
 
 //Offers selected by the user
 
@@ -32,86 +33,77 @@ class DisplayDiscountsState extends State<DisplayDiscounts> {
             vertical: screenheight / 80, horizontal: screenwidth / 80),
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: screenwidth / 80, vertical: screenheight / 80),
-              child: Text(
-                'Available Discounts',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  fontFamily: 'Source Sans Pro',
-                ),
+            Expanded(   // To avoid renderflow in small device sizes , expanded widget is used
+              flex:6,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenwidth / 80, vertical: screenheight / 80),
+                    child: Text(
+                      'Available Discounts',
+                      style: CustomTextStyle(fontSize: 30.0),
+                    ),
+                  ),
+                  CustomDivider(height: screenheight / 100, width: screenwidth / 50),
+                  SizedBox(height: screenheight / 100),
+                  AllDiscounts(),
+                  SizedBox(
+                      height: screenheight / 50),
+                ],
               ),
             ),
-            Divider(
-              color: Colors.brown,
-              height: screenheight / 100,
-              thickness: screenheight / 100,
-              indent: screenwidth / 50,
-              endIndent: screenwidth / 50,
-            ),
-            SizedBox(height: screenheight / 100),
-            AllDiscounts(),
-            SizedBox(
-                height: screenheight /
-                    50), //So that the last discount does not get hidden behind the floating button
+            //So that the last discount does not get hidden behind the floating button
 
             data == null
                 ? Container()
                 : // if data is null , this means that the user has come to this page only to see the discounts so buttons for payment should not appear
-                Stack(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: FloatingActionButton.extended(
-                          heroTag: 'Discounts',
-                          icon: Icon(Icons.money_off),
-                          label: Text(
-                            'Get Discounts',
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w900,
-                              fontFamily: "Source Sans Pro",
+                Expanded(
+                  flex:1,
+                  child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: FloatingActionButton.extended(
+                            heroTag: 'Discounts',
+                            icon: Icon(Icons.money_off),
+                            label: Text(
+                              'Get Discounts',
+                              style: CustomTextStyle( fontWeight: FontWeight.w900),
                             ),
+                            onPressed: () {
+                            }, // resource picker url is launched
+                            backgroundColor: Colors.lightBlueAccent,
                           ),
-                          onPressed: () {}, // resource picker url is launched
-                          backgroundColor: Colors.lightBlueAccent,
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: FloatingActionButton.extended(
-                          heroTag: 'Payment',
-                          icon: Icon(Icons.arrow_forward),
-                          label: Text(
-                            'Go to Payment',
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w900,
-                              fontFamily: "Source Sans Pro",
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: FloatingActionButton.extended(
+                            heroTag: 'Payment',
+                            icon: Icon(Icons.arrow_forward),
+                            label: Text(
+                              'Go to Payment',
+                              style: CustomTextStyle(fontWeight: FontWeight.w900),
                             ),
+                            onPressed: () {
+                              //pops the current page
+                              Navigator.pop(context);
+                              //Pops the previous page in the stack which is choose_policy page.
+                              //For now all these arguments are  send to the home page
+                              Navigator.pop(context, {
+                                'selectedOffer': selectedOffer,
+                                'selectedPolicy': data['selectedPolicy'],
+                                'userAddress': data['userAddress'],
+                              });
+                            },
+                            backgroundColor: Colors.lightBlueAccent,
                           ),
-                          onPressed: () {
-                            //pops the current page
-                            Navigator.pop(context);
-                            //Pops the previous page in the stack which is choose_policy page.
-                            //For now all these arguments are  send to the home page
-                            Navigator.pop(context, {
-                              'selectedOffer': selectedOffer,
-                              'selectedPolicy': data['selectedPolicy'],
-                              'userAddress': data['userAddress'],
-                            });
-                          },
-                          backgroundColor: Colors.lightBlueAccent,
+
                         ),
 
-                      ),
-
-                    ],
-                  )
+                      ],
+                    ),
+                )
 
           ],
         ),
@@ -174,9 +166,6 @@ class _AllDiscountsState extends State<AllDiscounts> {
                                     (entry) => Align(
                                       alignment: Alignment.topLeft,
                                       child: Container(
-//                                          margin: EdgeInsets.symmetric(
-//                                              vertical: screenheight / 100,
-//                                              horizontal: screenwidth / 100),
                                           padding: EdgeInsets.symmetric(
                                               vertical: screenheight / 80,
                                               horizontal: screenwidth / 80),
@@ -184,12 +173,9 @@ class _AllDiscountsState extends State<AllDiscounts> {
                                           child: Text(
                                             '${entry.key} : ${entry.value}',
                                             textAlign: TextAlign.left,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: "Source Sans Pro",
-                                              fontSize: 17,
-                                            ),
-                                          )),
+                                            style: CustomTextStyle(fontSize: 17),
+                                          ),
+                                      ),
                                     ),
                                   )
                                   .toList()),
@@ -199,10 +185,7 @@ class _AllDiscountsState extends State<AllDiscounts> {
                           child: Text(
                             '${CompanyDataBase.availableOffers[index].discount} %',
                             textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20.0,
-                            ),
+                            style: CustomTextStyle( fontWeight: FontWeight.w500, fontSize: 20.0),
                           ),
                         )
                       ],
