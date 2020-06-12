1a6d659528d87958bbc06e8ac5786f6c414d3f1a
oshhh <osheen@google.com>
2020-06-12 23:28:08 +0530

    only include active policies

--- home_insurance_app/lib/data/database_utilities.dart
+++ home_insurance_app/lib/data/database_utilities.dart
@@ -3,13 +3,15 @@ import 'package:homeinsuranceapp/data/policy.dart';
 
 Future<List<Policy>> getPolicies(int pincode) async {
   int category  = getCategoryFromPincode(pincode);
-  QuerySnapshot snapshot = await Firestore.instance.collection('policy').where('category', isEqualTo: category).getDocuments();
+  QuerySnapshot snapshot = await Firestore.instance.collection('policy').where('category', isEqualTo: category).where('deactivated', isEqualTo: false).getDocuments();
   List<Policy> list = [];
   for(var doc in snapshot.documents) {
     list.add(new Policy(doc.data['name'], doc.data['validity'], doc.data['cost']));
   }
   return list;
 }
+
+
 
 int getCategoryFromPincode(pincode) {
   // Implement business logic of how to assign categories to pincodes
