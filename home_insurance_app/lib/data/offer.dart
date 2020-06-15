668ec91600395f7a1f167c441ef47329d70ec208
Khushi Gupta <59967262+khushi2305@users.noreply.github.com>
2020-06-15 17:10:22 +0530

    Show_discounts_Pull1[P0] (#41)
    Added a page for Showing all the discount offers . The user can select one of the offers . The page has 2 buttons , one for launching resource picker and other for payment .

---
+++ home_insurance_app/lib/data/offer.dart
@@ -0,0 +1,10 @@
+//This class is a structure for each discount offer given by the company
+class Offer {
+  Map<String, int>
+      requirements; // This map stores each device type and their corresponding number required for this offer
+  int discount;
+  Offer(Map<String, int> requirements, int discount) {
+    this.requirements = requirements;
+    this.discount = discount;
+  }
+}
