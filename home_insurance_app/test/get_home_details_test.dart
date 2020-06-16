3008eb902a705349ad4f84bd0838659a369b5be7
khushi2305 <guptakhushi2402@gmail.com>
2020-06-16 10:30:44 +0530

    Added keys to widgets of get_home_details.dart

--- home_insurance_app/test/get_home_details_test.dart
+++ home_insurance_app/test/get_home_details_test.dart
@@ -12,12 +12,12 @@ void main() {
 
     // Test for presence and tapping of  the Submit Button
     // Since the form will not be submitted , tapping submit button will not take us to other page
-    final submitButton = find.text("Submit");
+    final submitButton = find.text("SUBMIT");
     expect(submitButton, findsOneWidget);
     await tester.tap(submitButton);
     await tester.pumpAndSettle();
 
-    //Check that in all input fields text could be entered
+//    //Check that in all input fields text could be entered
     await tester.enterText(
         find.byKey(Key('First Address Line')), 'A-123 , Street Name');
     await tester.enterText(find.byKey(Key('Second Address Line')), 'Rohini');
